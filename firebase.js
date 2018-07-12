9e7a4ed260a37c17f5331bfc748b2bb6bb37779f
nitin dhemiwal <dhemiwal.nitin@gmail.com>
2018-07-12 18:55:52 +0530

    adding the pages

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var app_firebase={};
+(function (){ var config = {
+    apiKey: "AIzaSyAWuDDbNYtiwJAJUhGfUD_cOymI-ZNqssA",
+    authDomain: "fir-authentication-eb9d4.firebaseapp.com",
+    databaseURL: "https://fir-authentication-eb9d4.firebaseio.com",
+    projectId: "fir-authentication-eb9d4",
+    storageBucket: "fir-authentication-eb9d4.appspot.com",
+    messagingSenderId: "282743532323"
+};
+    firebase.initializeApp(config);
+app_firebase=firebase;
+})()
